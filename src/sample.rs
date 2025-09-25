98f4c95cb4cf273abdc4130ba9fab6652e3b60b9
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-09-24 21:19:52 -0400

    fix: mismatched_lifetime_syntaxes

--- src/sample.rs
+++ src/sample.rs
@@ -14,7 +14,7 @@ pub enum SampleSize {
     All,
 }
 
-pub fn take_sample_from_start<R>(reader: &mut R, sample_size: SampleSize) -> Result<SampleIter<R>>
+pub fn take_sample_from_start<R>(reader: &mut R, sample_size: SampleSize) -> Result<SampleIter<'_, R>>
 where
     R: Read + Seek,
 {
